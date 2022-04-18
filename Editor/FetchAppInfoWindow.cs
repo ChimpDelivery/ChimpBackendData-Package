8234604a1072538ddbc067881156eec17d315e22
Emre Kovanci <kovanciemre@gmail.com>
2022-04-18 22:29:29 +0300

    up

--- Editor/FetchAppInfoWindow.cs
+++ Editor/FetchAppInfoWindow.cs
@@ -7,10 +7,11 @@ namespace TalusBackendData
 {
     public class FetchAppInfoWindow : EditorWindow
     {
-        private const string API_URL = "http://dashboard.talusstudio.eu.ngrok.io";
+        private string _ApiUrl = "http://dashboard.talusstudio.eu.ngrok.io";
+        private string _ApiToken;
 
         private string _AppId;
-        private string _ApiToken;
+
         private string _FetchButton = "Fetch";
 
         [MenuItem("TalusKit/Backend/Fetch App Info")]
@@ -26,12 +27,16 @@ namespace TalusBackendData
             GUILayout.BeginVertical();
 
             GUILayout.Label("API Settings", EditorStyles.boldLabel);
-            _AppId = EditorGUILayout.TextField("App ID:", _AppId);
+            _ApiUrl = EditorGUILayout.TextField("Api Url:", _ApiUrl);
             _ApiToken = EditorGUILayout.TextField("Api Key:", _ApiToken);
+
+            GUILayout.FlexibleSpace();
+            GUILayout.Label("App Settings", EditorStyles.boldLabel);
+            _AppId = EditorGUILayout.TextField("App ID:", _AppId);
 
             if (GUILayout.Button(_FetchButton))
             {
-                new FetchAppInfo(API_URL, _ApiToken, _AppId).GetInfo(UpdateBackendData);
+                new FetchAppInfo(_ApiUrl, _ApiToken, _AppId).GetInfo(UpdateBackendData);
             }
 
             GUILayout.EndVertical();
