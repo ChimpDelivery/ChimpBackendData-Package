4e22e4151e009aa79fcb835ad9a7565634f813df
Emre Kovanci <kovanciemre@gmail.com>
2022-06-29 16:00:12 +0300

    refactor utility

--- Editor/Utility/InfoBox.cs
+++ Editor/Utility/InfoBox.cs
@@ -4,14 +4,6 @@ namespace TalusBackendData.Editor.Utility
 {
     public static class InfoBox
     {
-        public static void Show(string title, string message, string ok, string close = null, System.Action okAction = null)
-        {
-            if (EditorUtility.DisplayDialog(title, message, ok, close))
-            {
-                okAction?.Invoke();
-            }
-        }
-
         public static void ShowBackendParameterError(string parameterName)
         {
             Show(
@@ -25,11 +17,15 @@ namespace TalusBackendData.Editor.Utility
 
         public static void ShowConfirmation(string message, System.Action okAction = null)
         {
-            if (EditorUtility.DisplayDialog("Are you sure ?", message, "Yes, I know", "Cancel"))
+            Show("Are you sure?", message, "Yes, I know", "Cancel", okAction);
+        }
+
+        public static void Show(string title, string message, string ok, string close = null, System.Action okAction = null)
+        {
+            if (EditorUtility.DisplayDialog(title, message, ok, close))
             {
                 okAction?.Invoke();
             }
         }
-
     }
 }
