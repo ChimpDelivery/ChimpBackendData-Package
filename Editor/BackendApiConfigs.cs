58b15cb2a1b36c055862a739a03a3860e3258940
Emre Kovanci <kovanciemre@gmail.com>
2022-12-01 03:02:36 +0300

    up

--- Editor/BackendApiConfigs.cs
+++ Editor/BackendApiConfigs.cs
@@ -9,8 +9,8 @@ namespace TalusBackendData.Editor
         public string ProvisionUuidKey = "Dashboard-Provision-Profile-UUID";
         public string TeamIdKey        = "Dashboard-Team-ID";
 
-        public string ArtifactFolder => Path.Combine(Directory.GetCurrentDirectory(), "Builds/");
-        public string TempFile => $"{ArtifactFolder}/dashboard-temp-file";
+        public string ArtifactFolder => Directory.GetCurrentDirectory() + "/Builds";
+        public string TempFile => ArtifactFolder + "/dashboard-temp-file";
 
         public static BackendApiConfigs GetInstance()
         {
