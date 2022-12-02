9da0885960f54a2bad2d79d103dd455751072094
Emre Kovanci <kovanciemre@gmail.com>
2022-12-02 09:37:59 +0300

    up

--- Editor/AssetProvider/BaseProvider.cs
+++ Editor/AssetProvider/BaseProvider.cs
@@ -2,7 +2,7 @@ namespace TalusBackendData.Editor.AssetProvider
 {
     public abstract class BaseProvider
     {
-        public bool IsCompleted { get; set; }
+        public bool IsCompleted { get; protected set; }
 
         public abstract void Provide();
     }
