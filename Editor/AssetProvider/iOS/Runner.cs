55999f8e535576508277b926447e04beaffa1872
Emre Kovanci <kovanciemre@gmail.com>
2022-12-02 09:30:54 +0300

    refactor

--- Editor/AssetProvider/iOS/Runner.cs
+++ Editor/AssetProvider/iOS/Runner.cs
@@ -1,41 +1,28 @@
+using System.Linq;
 using System.Threading;
 using System.Collections.Generic;
 
 using TalusBackendData.Editor.Utility;
 
+// ReSharper disable UnusedType.Global
+
 namespace TalusBackendData.Editor.AssetProvider.iOS
 {
     public static class Runner
     {
-        private static readonly List<BaseProvider> providers = new()
+        private static readonly List<BaseProvider> _Providers = new()
         {
             new VersionSettingsProvider(),
             new ProductSettingsProvider(),
-            new ProvisionProvider(),
+            new ProvisionProvider()
         };
 
-        public static bool IsSatisfy
-        {
-            get
-            {
-                int satisfied = 0;
-
-                for (int i = 0; i < providers.Count; ++i)
-                {
-                    if (providers[i].IsCompleted)
-                    {
-                        ++satisfied;
-                    }
-                }
-
-                return satisfied == providers.Count;
-            }
-        }
+        public static bool IsSatisfy => _Providers.Count(t => t.IsCompleted) == _Providers.Count;
 
         // Jenkins execute this function as a stage
         public static void CollectAssets()
         {
-            providers.ForEach(provider => provider.Provide());
+            _Providers.ForEach(provider => provider.Provide());
 
             while (!IsSatisfy)
             {
