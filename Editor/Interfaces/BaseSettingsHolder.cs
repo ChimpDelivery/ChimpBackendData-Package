d5c09fe90b31e272959d0c5578fabc31618a29c7
Emre Kovanci <kovanciemre@gmail.com>
2023-07-12 21:05:34 +0300

    rm unnec

--- Editor/Interfaces/BaseSettingsHolder.cs
+++ Editor/Interfaces/BaseSettingsHolder.cs
@@ -6,8 +6,6 @@ namespace ChimpBackendData.Editor.Interfaces
 {
     public abstract class BaseSettingsHolder<T> : ScriptableSingleton<T> where T : ScriptableObject
     {
-        public string Path => GetFilePath();
-
         public void SaveSettings()
         {
             Save(true);
